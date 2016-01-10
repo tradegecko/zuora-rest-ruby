03a3f8cd5490aae8ee1c3accd7bee8ed8953a8c3
Gherry Harahap <gherry.harahap@gmail.com>
2016-01-10 21:22:06 +0800

    Create and update separate body and options

--- lib/zuora/rest_operations/create.rb
+++ lib/zuora/rest_operations/create.rb
@@ -1,8 +1,8 @@
 module Zuora
   module RESTOperations
     module Create
-      def create(params={})
-        Zuora.request(:post, base_resource_url, params)
+      def create(body={}, params={})
+        Zuora.request(:post, base_resource_url, params.merge({body: body.to_json}))
       end
     end
   end
--- lib/zuora/rest_operations/update.rb
+++ lib/zuora/rest_operations/update.rb
@@ -1,8 +1,8 @@
 module Zuora
   module RESTOperations
     module Update
-      def update(params={})
-        Zuora.request(:put, base_resource_url, params)
+      def update(body={}, params={})
+        Zuora.request(:put, base_resource_url, params.merge({body: body.to_json}))
       end
     end
   end
