5565f645da4afa31568de2d839951d95bf5127f0
Thomas Hoen <tom@givecorps.com>
2018-03-21 18:55:26 -0400

    adds the oauth_token class

---
+++ lib/zuora/oauth_token.rb
@@ -0,0 +1,8 @@
+module Zuora
+  class OauthToken < Resource
+    extend Zuora::RESTOperations::Create
+
+  end
+end
+
+client_id: 'aebf10f8-f6dd-4e74-84d9-58ea1e352008', client_secret: 'A8X+y7HJXxJ++ysyNra9'
