7f0ff87fdb8c5ff0bd72feae1cfe7c09aee0e3a8
Thomas Hoen <tom@givecorps.com>
2018-04-02 22:13:26 -0400

    Add bearer token and ability to set it in an authorization header

--- lib/zuora-rest.rb
+++ lib/zuora-rest.rb
@@ -27,27 +27,28 @@ require "zuora/payment_methods/credit_card"
 require "zuora/catalog/product"
 require "zuora/charge_revenue_summaries/subscription_charge"
 require "zuora/rsa_signature"
+require "zuora/oauth_token"
 require "zuora/access_token"
 require "zuora/data_query"
 
 module Zuora
   class << self
-    attr_accessor :bearer_token, :production_mode, :debug_output
+    attr_accessor :production_mode, :debug_output, :client_id, :client_secret
 
     def base_url
       if production_mode
-        "https://rest.zuora.com"
+        "https://rest.zuora.com/"
       else
-        "https://rest.apisandbox.zuora.com"
+        "https://apisandbox-api.zuora.com/rest/"
       end
     end
-
+    
     def api_url
-      if production_mode
-        "https://rest.zuora.com/v1/"
-      else
-        "https://rest.apisandbox.zuora.com/v1/"
-      end
+        if production_mode
+            "https://rest.zuora.com/v1/"
+            else
+            "https://rest.apisandbox.zuora.com/v1/"
+        end
     end
 
     def payment_page_url
@@ -59,11 +60,28 @@ module Zuora
     end
 
     def request(method, url, params={})
+      attempts ||= 0
       response = Zuora::HttpClient.public_send(method, url, params.merge(options))
       Zuora::ErrorHandler.handle_response(response)
       # below is from the upstream gem. TODO: need to investigate
       # error_handler = pick_error_handler(params)
       # error_handler.handle_response(response)
+    rescue Zuora::APIError => e
+      if e.message.match("Error 90000011") #"Error 90000011: This resource is protected, please sign in first"
+        # since zuora does not believe we have rights to get information from it
+        # likely the bearer token has expired. So let's get a new one and
+        # set it in the header
+        Zuora::HttpClient.replace_bearer_token
+        Zuora::HttpClient.set_authorization_header
+        sleep 1
+        if (attempts += 1) <= 2
+          retry
+        else
+          raise
+        end
+      else
+        raise # since it was  not the protected resource error, we want to re-raise because it could be that Zuora is down
+      end
     end
 
     def options
