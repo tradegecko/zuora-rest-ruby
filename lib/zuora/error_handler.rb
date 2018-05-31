b1a0dea62780d9143d88e3f88451d06116ea8534
Thomas Hoen <tom@givecorps.com>
2018-05-31 08:41:47 -0400

    actions return a differently formatted packet, where everything is wrapped in a results array and success is capitilized

--- lib/zuora/error_handler.rb
+++ lib/zuora/error_handler.rb
@@ -11,9 +11,16 @@ module Zuora
 # TODO Here we have a conflict, because our version has a ErrorHandler class, and the upstream version has an ErrorHandler module. 
   class ErrorHandler
     def self.handle_response(response)
-      return formatted_response(response.except("success")) if response["success"]
+      # the response may be nested in a results array and have a different success key
+      if !response["results"].nil?
+        success_key = "Success"
+        response = response["results"].first
+      else
+        success_key = "success"
+      end
+      return formatted_response(response.except(success_key)) if response[success_key]
 
-      if response["success"].nil?
+      if response[success_key].nil?
         raise Zuora::UnknownError.new(response)
       else
         reason = humanize_reason(response)
@@ -32,9 +39,18 @@ module Zuora
   private
 
     def self.humanize_reason(response)
-      response["reasons"].map do |reason_hash|
-        "Error #{reason_hash['code']}: #{reason_hash['message'].humanize}"
-      end.join("\n")
+      if !response["reasons"].nil?
+        return response["reasons"].map do |reason_hash|
+          "Error #{reason_hash['code']}: #{reason_hash['message'].humanize}"
+        end.join("\n")
+      elsif !response["Errors"].nil?
+        return response["Errors"].map do |reason_hash|
+          "Error #{reason_hash['Code']}: #{reason_hash['Message'].humanize}"
+        end.join("\n")
+      else
+        "No error reason was provided"
+      end
+
     end
 
   end
