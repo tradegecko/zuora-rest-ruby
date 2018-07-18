e43ae2d28b1aba3a3a7a2a34ca43da6dcd7ec23a
Thomas Hoen <tom@givecorps.com>
2018-07-18 14:55:08 -0400

    Adds methods to retrieve all payment methods by account

--- lib/zuora/payment_method.rb
+++ lib/zuora/payment_method.rb
@@ -1,5 +1,15 @@
 module Zuora
   class PaymentMethod < Resource
     extend Zuora::RESTOperations::Delete
+    class << self
+
+      def all_by_account_id(account_id)
+        Zuora.request(:get, all_by_account_id_url(account_id), {})
+      end
+
+      def all_by_account_id_url(account_id)
+        base_resource_url + "/credit-cards/accounts/#{account_id}"
+      end
+    end
   end
 end
