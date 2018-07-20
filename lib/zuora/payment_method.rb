403ed9d6e15a201ca22119d0fbb2541f3a3075fa
Thomas Hoen <tom@givecorps.com>
2018-07-20 12:06:09 -0400

    Move the update and the list of payment methods by account out of the payment method class and into a credit card class that is namespaced under PaymentMethods

--- lib/zuora/payment_method.rb
+++ lib/zuora/payment_method.rb
@@ -1,18 +1,6 @@
 module Zuora
   class PaymentMethod < Resource
     extend Zuora::RESTOperations::Delete
-    extend Zuora::RESTOperations::Update
-    extend Zuora::RESTOperations::Find
 
-    class << self
-
-      def all_by_account_id(account_id)
-        Zuora.request(:get, all_by_account_id_url(account_id), {})
-      end
-
-      def all_by_account_id_url(account_id)
-        base_resource_url + "/credit-cards/accounts/#{account_id}"
-      end
-    end
   end
 end
